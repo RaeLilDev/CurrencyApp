b28a02fb60daaaf96919a6e142e7f382b2e5c487
CoRae <raelilxtet@gmail.com>
2022-12-10 11:36:06 +0630

    Update Api Key

--- CurrencyApp/Utils/AppConstants.swift
+++ CurrencyApp/Utils/AppConstants.swift
@@ -9,6 +9,6 @@ import Foundation
 
 struct AppConstants {
     static let baseURL = "https://api.apilayer.com/currency_data/"
-    static let apiKey = "MxoqGt8OQ6Vee1l00AJzumOsJQrX8We6"
+    static let apiKey = "ndRycHXFEQefpTR6LJEAMpjiKjMWF3k1"
     static let updatedTime = "updatedTime"
 }
